2b121e0beb4a656298c23e4b0b1874e0b9452f83
benaitcheson <benaitcheson@hotmail.com>
2021-07-31 22:45:56 +1000

    made created some routes for the different web pages

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -1,34 +1,61 @@
 class PlannerController < ApplicationController
 
-    before_action :read_planner
+
+
+    # before_action :read_planner
     skip_before_action :verify_authenticity_token
 
     def index
-        render json: @planner
+        render :index
     end
 
-    def show
-        found_planner = @planner.find do |planners|
-            planners["id"] == params[:id].to_i
-        end
+    def aboutus
 
-        render json: found_planner
     end
 
-    def create
-        new_planner = { id: params[:id].to_i, name: params[:name] }
-        @planner << new_planner
-        write_planner(@planner)
-        redirect_to planner_path
+    def howto
+
     end
 
-    private
+    def new
 
-    def write_planner(planner)
-        File.write(Rails.public_path.join("planners.json"), JSON.generate(@planner))
     end
 
-    def read_planner
-        @planner = JSON.parse(File.read(Rails.public_path.join("planners.json")))
+    def products
+
     end
+
+    def register
+
+    end
+    
+
+    # def new
+    #     render "planner/index"
+    # end
+
+    # def show
+    #     found_planner = @planner.find do |planners|
+    #         planners["id"] == params[:id].to_i
+    #     end
+
+    #     render json: found_planner
+    # end
+
+    # def create
+    #     new_planner = { id: params[:id].to_i, name: params[:name] }
+    #     @planner << new_planner
+    #     write_planner(@planner)
+    #     redirect_to planner_path
+    # end
+
+    # private
+
+    # def write_planner(planner)
+    #     File.write(Rails.public_path.join("planners.json"), JSON.generate(@planner))
+    # end
+
+    # def read_planner
+    #     @planner = JSON.parse(File.read(Rails.public_path.join("planners.json")))
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,33 @@
 Rails.application.routes.draw do
+
+  # needs a root directory
+  # root to:
+
+  # view
   get "/planner", to: "planner#index"
 
-  post "/planner", to: "planner#create"
+  get "/planner/new", to: "planner#new"
 
-  get "/planner/:id", to: "planner#show"
+  get "/planner/:id", to: "planner#hide"
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # # controller
+  
+  # post "/planner", to: "planner#create"
+
+  # get "/planner/:id", to: "planner#show"
+
+  
+
 
 end
