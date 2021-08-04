04121481284c301158a09c10da7b90f92f3b8a3d
benaitcheson <benaitcheson@hotmail.com>
2021-08-04 20:55:39 +1000

    trying to solve bootstrap problem

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -30,28 +30,28 @@ class PlannerController < ApplicationController
     end
     
     # method for Stripe
-    def show
-        session = Stripe::Checkout::Session.create(
-            payment_method_types: ['card'],
-            customer_email: "benaitcheson@hotmail.com", # this is hardcoded
-            line_items: [{ 
-                name: @event.name,
-                description: @event.description,
-                images: [], # this is hardcoded
-                amount: (@event.price * 100).to_i,
-                currency: 'AUD',
-                quantity: 1 # this is hardcoded
-             }],
-            payment_intent_data: { 
-                metadata: { 
-                    event_id: @event_id
-                 }
-            },
-            success_url: "#{root url}payments/success?eventId=#{@event.id}",
-            cancel_url: "#{root url}events"
-        )
-        @session_id = session.id
-    end
+    # def show
+    #     session = Stripe::Checkout::Session.create(
+    #         payment_method_types: ['card'],
+    #         customer_email: "benaitcheson@hotmail.com", # this is hardcoded
+    #         line_items: [{ 
+    #             name: @event.name,
+    #             description: @event.description,
+    #             images: [], # this is hardcoded
+    #             amount: (@event.price * 100).to_i,
+    #             currency: 'AUD',
+    #             quantity: 1 # this is hardcoded
+    #          }],
+    #         payment_intent_data: { 
+    #             metadata: { 
+    #                 event_id: @event_id
+    #              }
+    #         },
+    #         success_url: "#{root url}payments/success?eventId=#{@event.id}",
+    #         cancel_url: "#{root url}events"
+    #     )
+    #     @session_id = session.id
+    # end
 
     # def new
     #     render "planner/index"
