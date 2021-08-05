9ab171a6fa3c0eeff2ae362f634b3da7c7c3f9c6
benaitcheson <benaitcheson@hotmail.com>
2021-08-05 22:53:32 +1000

    made the navbar links in ruby

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
   devise_for :admins
   root to: "planner#index"
 
-  get "/register", to: "planner#register"
+  get "/index", to: "planner#index", as: :index
 
-  get "/index", to: "planner#index"
+  get "/howto", to: "planner#howto", as: :howto
 
-  get "/howto/", to: "planner#howto"
+  get "/products", to: "planner#products", as: :products
 
-  get "/products", to: "planner#products"
+  get "/admins/sign_in", to: "admin#sign_in", as: :sign_in
+
+  get "/admins/sign_up", to: "admin#sign_up", as: :sign_up
 
 end
