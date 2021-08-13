c0b5b1b376cf5f34e61cef56d0c0058a9059fb01
benaitcheson <benaitcheson@hotmail.com>
2021-08-13 23:04:43 +1000

    added listing methods from coder app example

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -1,9 +1,107 @@
 class ClientController < ApplicationController
 
-    def dashboard
+    before_action :set_listing, only: %i[ show edit update destroy ]
+    skip_before_action :verify_authenticity_token, only: [:show]
+    before_action :authenticate_user! 
+
+    # GET /listings or /listings.json
+    def index
+        @listings = Listing.all
     end
 
-    def new_advice
+    def show
+        session = Stripe::Checkout::Session.create(
+        payment_method_types: ['card'],
+        customer_email: current_admin.email,
+        line_items: [{
+            name: @listing.name,
+            description: @listing.description,
+            images: [""],
+            amount: (@listing.price * 100).to_i,
+            currency: 'aud',
+            quantity: 1
+        }],
+        payment_intent_data: {
+            metadata: {
+            listing_id: @listing.id,
+            user_id: current_admin.id
+            }
+        },
+        success_url: "#{root_url}payments/success?eventID=#{@listing.id}",
+        cancel_url: "#{root_url}listings"
+        )
+
+        @session_id = session.id
+        pp session
     end
 
+    # GET /listings/new
+    def new
+        @listing = Listing.new
+    end
+
+    # GET /listings/1/edit
+    def edit
+        authorize @listing
+    end
+
+    # POST /listings or /listings.json
+    def create
+        @listing = Listing.new(listing_params)
+        @listing.admin_id = current_admin.id
+        respond_to do |format|
+            if @listing.save
+            format.html { redirect_to @listing, notice: "Listing was successfully created." }
+            format.json { render :show, status: :created, location: @listing }
+            else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @listing.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    # PATCH/PUT /listings/1 or /listings/1.json
+    def update
+        respond_to do |format|
+            if @listing.update(listing_params)
+            format.html { redirect_to @listing, notice: "Listing was successfully updated." }
+            format.json { render :show, status: :ok, location: @listing }
+            else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @listing.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    # DELETE /listings/1 or /listings/1.json
+    def destroy
+        @listing.destroy
+        respond_to do |format|
+            format.html { redirect_to listings_url, notice: "Listing was successfully destroyed." }
+            format.json { head :no_content }
+    end
+
+        @listing = Listing.find(params[:id])
+        authorize @listing
+
+        if @listing.destroy
+            flash[:notice] = "\"#{@listing.name}\" was successfully deleted."
+            redirect_to admin_path
+        else
+            flash.now[:alert] = "Error"
+            render :show
+        end
+    end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_listing
+        @listing = Listing.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def listing_params
+        params.require(:listing).permit(:ppor, :contents, :investproperty, :business, :collectable, :share, :cash, :vehicles, :other)
+    end
 end
--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -1,7 +1,5 @@
 class PlannerController < ApplicationController
 
-
-
     # before_action :read_planner
     skip_before_action :verify_authenticity_token
 
@@ -16,29 +14,5 @@ class PlannerController < ApplicationController
     def products
 
     end
-    
-    # method for Stripe
-    # def show
-    #     session = Stripe::Checkout::Session.create(
-    #         payment_method_types: ['card'],
-    #         customer_email: "benaitcheson@hotmail.com", # this is hardcoded
-    #         line_items: [{ 
-    #             name: @event.name,
-    #             description: @event.description,
-    #             images: [], # this is hardcoded
-    #             amount: (@event.price * 100).to_i,
-    #             currency: 'AUD',
-    #             quantity: 1 # this is hardcoded
-    #          }],
-    #         payment_intent_data: { 
-    #             metadata: { 
-    #                 event_id: @event_id
-    #              }
-    #         },
-    #         success_url: "#{root url}payments/success?eventId=#{@event.id}",
-    #         cancel_url: "#{root url}events"
-    #     )
-    #     @session_id = session.id
-    # end
 
 end
